0f3d478e5eef5547d3553aa5b9a95782bde65691
mirandaester08 <159536753+mirandaester08@users.noreply.github.com>
2024-03-15 08:37:01 -0300

    salvar

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let cor;
+let posicaoHorizontal; // x
+
+llet posicaoVertical; // y
+
+function setup() {
+  createCanvas(900, 900);
+   background(color(00,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+
+function draw() {
+ 
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,90);
+ 
+ 
+ 
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
+  }
+ 
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
+  }
+ 
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+   
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+ 
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+   
+}
